6eb0fb95103a19cc45cadd62fd1f75c62441bc3f
aliselmes <a.selmes@gmail.com>
2021-03-02 20:38:39 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Book from './components/Book';
+import Search from './components/Search';
+
+const BOOK_API_URL = ""
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello</h1>
   );
 }
 
---
+++ src/components/Book.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+
+const Book = ({book}) => {
+    return(
+        <div>
+            <Card>
+                <CardImg top width="100%" src={book.volumeInfo.imageLinks.thumbnail} alt={`Cover image of ${book.volumeInfo.title}`} />
+                <CardBody>
+                    <CardTitle tag="h5">{book.volumeInfo.title}</CardTitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">{book.volumeInfo.authors}</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                </CardBody>
+            </Card>
+        </div>
+    );
+};
+
+export default Book;
